9dbe716b92ff797fba929fd10e31ba9b90be4172
AlcoderDeena <133322685+AlcoderDeena@users.noreply.github.com>
2023-05-13 20:18:28 +0530

    Add files via upload

---
+++ src/pages/Front.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Front() {
+  return (
+    <div>
+      <p>
+        <h1>Welcome to our Employee App!</h1> 
+      </p>
+      <p>
+       <h4>This app is designed to streamline your work experience and make your day-to-day tasks easier.
+With the Employee App, you can access your work schedule, communicate with your colleagues, request time off, view your pay stubs and much more, all in one convenient location.
+</h4>
+    <p><h5>Some of the key features of the app include:</h5>
+    
+    <div><h6><ul>
+    <li>A personalized dashboard that provides you with a quick overview of your schedule, tasks, and messages.</li>
+    <li>An integrated messaging system that allows you to communicate with your colleagues in real-time.</li>
+    <li>A calendar that displays your upcoming shifts and events.</li>
+    <li>A time-off request system that enables you to request time off and view your current time-off balance.</li>
+    <li>A pay stubs section that provides you with access to your current and past pay stubs.</li>
+    <li>An employee directory that allows you to easily find contact information for your colleagues.</li></ul></h6></div></p>
+    <h4>
+   We understand the importance of having a user-friendly and efficient tool to manage your work responsibilities, and that's why we created the Employee App. We hope this app will help you stay organized, improve your productivity, and make your work experience more enjoyable.
+
+Thank you for choosing the Employee App, and we look forward to providing you with the best possible work experience.</h4>
+    </p>
+    </div>
+  )
+}
